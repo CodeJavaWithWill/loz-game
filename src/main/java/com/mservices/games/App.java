e99e9f95adba6862f8f38bbe096422e0beee9c4c
Wizzy Mt <fluids_herb0e@icloud.com>
2026-04-12 19:43:17 +0200

    structure change

---
+++ src/main/java/com/mservices/games/App.java
@@ -0,0 +1,9 @@
+package com.mservices.games;
+
+import com.mservices.games.core.QuestEngine;
+
+public class App {
+    public static void main(String[] args) {
+        QuestEngine.start();
+    }
+}
